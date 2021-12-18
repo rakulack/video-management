ad1f89b62e8c723c194cb583719292872cd67dfd
rakulack <rakui87@gmail.com>
2021-12-18 22:28:40 +0900

    create upload logic

--- video-management/src/main/java/com/rakulack/videomanagement/component/FileUploadComponent.java
+++ video-management/src/main/java/com/rakulack/videomanagement/component/FileUploadComponent.java
@@ -1,11 +1,14 @@
-package com.rakulack.videomanagement.component.impl;
+package com.rakulack.videomanagement.component;
+
+import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * upload file to server.
+ * 
  * @author rakulack
  */
 public interface FileUploadComponent {
-    String uploadFile(MultipartFile file);
+    String uploadFile(MultipartFile file) throws IOException;
 }
