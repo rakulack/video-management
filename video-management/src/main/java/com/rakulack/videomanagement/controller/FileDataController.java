50eeaa0e93c551ce7ed826dcf435140b6ad9d042
rakulack <rakui87@gmail.com>
2021-12-21 22:55:12 +0900

    maintain layout and create logic to filter nendo

--- video-management/src/main/java/com/rakulack/videomanagement/controller/FileDataController.java
+++ video-management/src/main/java/com/rakulack/videomanagement/controller/FileDataController.java
@@ -1,5 +1,8 @@
 package com.rakulack.videomanagement.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import com.rakulack.videomanagement.auth.SimpleLoginUser;
@@ -8,6 +11,7 @@ import com.rakulack.videomanagement.repository.FileInfoRepository;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,4 +27,21 @@ public class FileDataController {
     public List<FileInfo> getFileData(@AuthenticationPrincipal SimpleLoginUser loginUser) {
         return fileInfoRepository.findByUserIdOrderByPrcDateDesc(loginUser.getUser().getId());
     }
+
+    @GetMapping("/fileData/{YYYY}")
+    public List<FileInfo> getFileDataWithYear(@AuthenticationPrincipal SimpleLoginUser loginUser,
+            @PathVariable("YYYY") String yyyy) {
+        try {
+            String.valueOf(Integer.valueOf(yyyy).intValue() + 1);
+            SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+            Date sdate = sdFormat.parse(yyyy + "/04/01 00:00:00");
+            Date edate = sdFormat.parse(String.valueOf(Integer.valueOf(yyyy).intValue() + 1) + "/3/31 23:59:59");
+            return fileInfoRepository.findByUserIdAndFileDateBetweenOrderByFileDateDesc(loginUser.getUser().getId(),
+                    sdate, edate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- video-management/src/main/java/com/rakulack/videomanagement/repository/FileInfoRepository.java
+++ video-management/src/main/java/com/rakulack/videomanagement/repository/FileInfoRepository.java
@@ -1,5 +1,6 @@
 package com.rakulack.videomanagement.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import com.rakulack.videomanagement.entity.FileInfo;
@@ -10,4 +11,7 @@ public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
     FileInfo findByUserIdAndFileName(Long userId, String fileName);
 
     List<FileInfo> findByUserIdOrderByPrcDateDesc(Long userId);
+
+    List<FileInfo> findByUserIdAndFileDateBetweenOrderByFileDateDesc(Long userId, Date FileDateStart,
+            Date FileDateEnd);
 }
